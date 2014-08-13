c0b3e554899b240de5968cd0ecc372ec16d8a866
Doug Jones <doug@dfjones.net>
2014-08-12 22:16:53 -0400

    Exteremely basic server using martini.

---
+++ server/server.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/binding"
+	"io"
+	"io/ioutil"
+	"log"
+	"mime/multipart"
+	"net/http"
+	"sync"
+)
+
+type ImageForm struct {
+	ImageUpload *multipart.FileHeader `form:"image" binding:"required"`
+}
+
+type LatestImage struct {
+	bytes []byte
+	mutex *sync.Mutex
+}
+
+var latestImage *LatestImage
+
+func imagePostTemp(res http.ResponseWriter, log *log.Logger, imageForm ImageForm, errors binding.Errors) string {
+	if errors != nil {
+		log.Println("errors %s", errors)
+		return "error"
+	}
+
+	file, err := imageForm.ImageUpload.Open()
+	if err != nil {
+		log.Println("error: %s", err)
+	}
+	defer file.Close()
+
+	tempFile, err := ioutil.TempFile("/tmp", "solar")
+
+	io.Copy(tempFile, file)
+
+	return tempFile.Name()
+}
+
+func imagePostMem(imageForm ImageForm) {
+	file, err := imageForm.ImageUpload.Open()
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	latestImage.mutex.Lock()
+	defer latestImage.mutex.Unlock()
+	latestImage.bytes, err = ioutil.ReadAll(file)
+}
+
+func imageGet(res http.ResponseWriter) []byte {
+	latestImage.mutex.Lock()
+	defer latestImage.mutex.Unlock()
+	return latestImage.bytes
+}
+
+func main() {
+
+	m := martini.Classic()
+
+	latestImage = &LatestImage{nil, &sync.Mutex{}}
+
+	m.Post("/images", binding.MultipartForm(ImageForm{}), imagePostMem)
+	m.Get("/images", imageGet)
+
+	m.Run()
+}
